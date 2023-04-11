29ed4e5de8fa4ca58ecc7afaea1eeec7a5877d82
d.larin <larin.dn@icloud.com>
2023-04-11 23:46:15 +0400

    homework-1, added tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+test_item = Item("Холодильник", 25000, 15)
+
+
+def test_calculate_total_price():
+    total_price = test_item.calculate_total_price()
+    assert total_price == 375000
+
+
+def test_apply_discount():
+    Item.pay_rate = 0.15
+    test_item.apply_discount()
+    discount_price = test_item.price
+    assert discount_price == 3750
