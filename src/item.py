bd5942c6d8af2ed6375d1741446044680ce434e6
d.larin <larin.dn@icloud.com>
2023-04-23 21:31:36 +0400

    homework-2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +17,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
@@ -33,3 +37,41 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @property
+    def name(self):
+        """
+        Getter для param name
+        """
+
+        return self.__name
+
+    @name.setter
+    def name_length(self, new_name: str):
+        """
+        Проверяет длину наименования товара через param name.
+        Длина должна быть не более 10 символов.
+        """
+        if len(new_name) <= 10:
+            self.__name = new_name
+        else:
+            raise Exception('Длина наименования товара не может быть более 10-ти символов')
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv
+        """
+
+        cls.all = []
+        with open(os.path.join(os.path.dirname(__file__), 'items.csv'), newline='') as csvfile:
+            data = csv.DictReader(csvfile)
+            for row in data:
+                print(row)
+                cls(row['name'], cls.string_to_number(row['price']), cls.string_to_number(row['quantity']))
+
+    @staticmethod
+    def string_to_number(value: str):
+        if '.' in value:
+            return int(float(value))
+        return float(value)
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,9 +1,8 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-import pytest
-
 from src.item import Item
 
 test_item = Item("Холодильник", 25000, 15)
+over_ten_simbols = 'long_name_of_item'
 
 
 def test_calculate_total_price():
@@ -14,5 +13,15 @@ def test_calculate_total_price():
 def test_apply_discount():
     Item.pay_rate = 0.15
     test_item.apply_discount()
-    discount_price = test_item.price
+    discount_price = int(test_item.price)
     assert discount_price == 3750
+
+
+def test_string_to_number():
+    str_value = '10.1'
+    new_value = Item.string_to_number(str_value)
+    assert new_value == 10
+
+# def test_name_length():
+#     Item.name_length(over_ten_simbols)
+#     assert 'Длина наименования товара не может быть более 10-ти символов'
